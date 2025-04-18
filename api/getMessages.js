3d7e747ae91c9caac16d72389b202be02a675bc0
SasukeGh <162058638+SasukeGh@users.noreply.github.com>
2025-04-18 14:24:39 +0000

    Update getMessages.js

--- api/getMessages.js
+++ api/getMessages.js
@@ -1,1 +1,12 @@
+import { Client } from 'pg';
+
+export default async function handler(req, res) {
+  const client = new Client({ connectionString: process.env.PG_CONN_STRING });
+  await client.connect();
+
+  const result = await client.query('SELECT sender, message FROM messages ORDER BY created_at ASC');
+  await client.end();
+
+  res.status(200).json({ messages: result.rows });
+}
 
